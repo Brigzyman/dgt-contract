771a90dae843bfed522dc14f099756688925e2f6
Dmitry Dementyev-Dedelis <dmitry@digipulse.io>
2017-08-02 22:54:02 +0300

    Corrected comment latyout

--- contracts/DigiPulseToken.sol
+++ contracts/DigiPulseToken.sol
@@ -34,11 +34,11 @@ contract DigiPulseToken {
 
   // For future transfers of DGT
   function transferFrom(address _to, uint256 _value) {
-    require (balanceOf[msg.sender] >= _value);           // Check if the sender has enough
+    require (balanceOf[msg.sender] >= _value);          // Check if the sender has enough
     require (balanceOf[_to] + _value > balanceOf[_to]); // Check for overflows
 
-    balanceOf[msg.sender] -= _value;                        // Subtract from the sender
-    balanceOf[_to] += _value;                               // Add the same to the recipient
+    balanceOf[msg.sender] -= _value;                    // Subtract from the sender
+    balanceOf[_to] += _value;                           // Add the same to the recipient
 
     Transfer(msg.sender, _to, _value);
   }
